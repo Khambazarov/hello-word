f6b086c755550f39bc37ed58bc5f28e199abeea2
Khambazarov <khambazarovrenat@gmail.com>
2025-09-20 23:05:44 +0200

    add the names of month in circassian

--- frontend/src/utils/languageHelper.js
+++ frontend/src/utils/languageHelper.js
@@ -1,11 +1,13 @@
 import english from "../languages/english.json";
 import german from "../languages/german.json";
 import russian from "../languages/russian.json";
+import circassian from "../languages/circassian.json";
 
 const languages = {
   en: english,
   de: german,
   ru: russian,
+  ci: circassian,
 };
 
 export const getTranslations = (language) => {
